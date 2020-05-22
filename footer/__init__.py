d93f8f9e6b6e78b77ced7e16479f6f2fa94a243e
Rafael Miranda <rafael.effects@gmail.com>
2020-05-22 10:37:37 +0100

    Improved code, added one more test

--- footer/__init__.py
+++ footer/__init__.py
@@ -1,32 +1,55 @@
 def pagination(current_page=1, total_pages=10, boundaries=1, around=0):
-    left_boundary = []
+    left_boundary = [1]
     right_boundary = []
     around_pages = []
 
     if current_page > total_pages:
         return "Current page exceeds total number of pages"
 
-    for i in range(1, boundaries + 1):
-        left_boundary.append(i)
+    for i in range(0, boundaries + 1):
+        if i == 0:
+            continue
+        if i not in left_boundary:
+            left_boundary.append(i)
 
     for i in range(current_page - around, current_page + around + 1):
         around_pages.append(i)
 
-    for i in range(total_pages - boundaries, total_pages):
-        right_boundary.append(i+1)
+    for i in range(total_pages - boundaries, total_pages - 1):
+        if i > around_pages[-1]:
+            right_boundary.append(i+1)
 
-    left_boundary = [x for x in left_boundary if x not in around_pages]
-    around_pages = [x for x in around_pages if x not in right_boundary]
+    right_boundary.append(total_pages)
 
-    pages = left_boundary + around_pages + right_boundary
+    pages = left_boundary
     pagination = []
-    for i, page in enumerate(pages):
-        if i == 0:
-            pagination.append(str(page))
+
+    for i, page in enumerate(around_pages):
+        if i == 0 and around_pages[i] < total_pages:
+            pages.append(page)
             continue
+        if (around_pages[i] > total_pages):
+            break
+        if around_pages[i] not in pages:
+            pages.append(page)
+
+    for i, page in enumerate(right_boundary):
+        if i == 0 and right_boundary[i] < total_pages:
+            pages.append(page)
+            continue
+        if (right_boundary[i] > total_pages):
+            break
+        if right_boundary[i] not in pages:
+            pages.append(page)
+
+    pages.sort()
+
+    for i, page in enumerate(pages):
         if (pages[i] - pages[i - 1]) > 1:
             pagination.append("...")
-
-        pagination.append(str(page))
+        if (pages[i] > total_pages):
+            break
+        if pages[i] not in pagination:
+            pagination.append(page)
 
     return ' '.join(str(e) for e in pagination)
--- tests.py
+++ tests.py
@@ -24,11 +24,16 @@ class FooterTests(unittest.TestCase):
         '''Values from exercise example 2 should work'''
         self.assertEqual(pagination(4, 10, 2, 2), "1 2 3 4 5 6 ... 9 10")
 
+    def pagination_high_boundaries_value_should_not_break(self):
+        '''High boundaries value should not break'''
+        self.assertEqual(pagination(5, 10, 20, 0), "1 2 3 4 5 6 7 8 9 10")
+
 
 suite = unittest.TestSuite()
 suite.addTest(FooterTests("pagination_default"))
 suite.addTest(FooterTests(
     "pagination_current_page_should_not_exceeed_total_number_of_pages"))
 suite.addTest(FooterTests("pagination_exercise_example_1"))
+suite.addTest(FooterTests("pagination_exercise_example_2"))
 suite.addTest(FooterTests("pagination_exercise_example_2"))
 unittest.TextTestRunner(verbosity=2).run(suite)
